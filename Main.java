4cdaa40da03ef449685e46ab83b18d6dac440792
sharma-pranita <sharmapranita787008@gmail.com>
2025-02-26 04:09:54 +0530

    datatypes

---
+++ Main.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    //1
+        // int a = 8;
+        
+        // for(int i=a ; i<=70 ; i++){
+        //   if(a<=70){
+           
+        //     System.out.println(a);
+        //     a = a +2;
+        //    }
+        // }
+        
+       
+    //2
+        // float sum = 0 , count = 0;
+        // int[] array = {1,2,3,4};
+        // for(int i=0 ; i<array.length ; i++){
+       
+        //     sum = sum + array[i];
+        //   count++;
+         
+        // }
+        // float avg = sum/count;
+        // System.out.println("Average : " + avg);
+
+
+
+        //3
+      
+        // System.out.print("Enter a : ");
+        // float a = scanner.nextInt();
+        // System.out.print("Enter b: ");
+        // float b = scanner.nextInt();
+        // System.out.print("Enter c: ");
+        // float c = scanner.nextInt();
+
+        // double avg = (a+b+c)/3;
+
+       
+        // System.out.println("Average : " + avg);
+
+        //4
+        // System.out.println("Enter the value of pencil :");
+        // float pencil = scanner.nextFloat();
+        // System.out.println("Enter the value of pen :");
+        // float pen = scanner.nextFloat();
+        // System.out.println("Enter the value of eraser :");
+        // float eraser = scanner.nextFloat();
+
+
+        
+        // float total = (pencil+pen+eraser);
+        // float totalCost =  total + +(18*(total)/100);
+        // System.out.println("Total cost : " + totalCost);
+
+
+
+        //5
+        // byte b = 4;
+        // char c = 'a';
+        // short s = 512;
+        // int i = 1000;
+        // float f = 3.14f;
+        // double d = 99.9954;
+      
+        // System.out.println(checkType((f*b)+(i%c)-(d*s)));
+
+      }
+}
+  
