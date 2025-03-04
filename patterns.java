986c7c98fcd98ae9930ec3f83e9f42b02ec2b538
sharma-pranita <sharmapranita707008@gmail.com>
2025-03-04 10:02:31 +0530

    patterns

---
+++ patterns.java
@@ -0,0 +1,60 @@
+public class patterns {
+
+  public static void main(String[] args) {
+    // for (int i = 1; i <= 4; i++) {
+    //   for(int j=1 ; j<=i ; j++){
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+
+    // }
+
+    // for (int i = 1; i <= 4; i++) {
+    //     for(int j=i ; j<=4 ; j++){
+    //       System.out.print("*");
+    //     }
+    //     System.out.println("");
+
+    //   }
+
+    // for(int i = 1; i<=4 ; i++){
+    //     for(int j=1 ; j<=i;j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+    //  char ch = 'A';
+    //  for(int line=1;line<=4;line++){
+    //   for(int chars=1;chars<=line;chars++){
+    //     System.out.print(ch);
+    //     ch++;
+    //   }
+    //   System.out.println();
+    //  }
+
+    // for(int i = 1; i <=4 ; i++){
+    //   for(int j =1 ; j<=5 ; j++){
+    //     if(i==1 || i==4 || j==1 || j==5){
+    //       System.out.print("*");
+    //     }
+    //     else{
+    //       System.out.print(" ");
+    //     }
+    //   }
+    //   System.out.println();
+
+    // }
+
+    // for (int i = 1; i <= 4; i++) {
+    //   for (int j = 4; j >= i; j--) {
+    //     if (j <= 0) {
+    //       break;
+    //     } else {
+    //       System.out.print("*");
+    //     }
+    //   }
+    //   System.out.println();
+    // }
+  }
+}
