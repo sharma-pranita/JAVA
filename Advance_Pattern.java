37c63582d90c4bca19f0ca6e7d7c6a0cf17e68a2
sharma-pranita <sharmapranita707008@gmail.com>
2025-03-09 22:22:07 +0530

    Patterns_code

--- Advance_Pattern.java
+++ Advance_Pattern.java
@@ -96,35 +96,35 @@ public class Advance_Pattern {
   //     }
   //   }
 
-  public static void Rhombus(int n) {
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n - i; j++) {
-        System.out.print(" ");
-      }
-      for (int j = 1; j <= i - 1; j++) {
-        System.out.print("*");
-      }
-      // for(int j=1;j<=;j++){
-      //     System.out.print("*");
-      // }
-      for(int k =1;k<=5;k++){
-        if(k==5){
-            System.out.print("*");
-        }
-      }
+  // public static void Rhombus(int n) {
+  //   for (int i = 1; i <= n; i++) {
+  //     for (int j = 1; j <= n - i; j++) {
+  //       System.out.print(" ");
+  //     }
+  //     for (int j = 1; j <= i - 1; j++) {
+  //       System.out.print("*");
+  //     }
+  //     // for(int j=1;j<=;j++){
+  //     //     System.out.print("*");
+  //     // }
+  //     for(int k =1;k<=5;k++){
+  //       if(k==5){
+  //           System.out.print("*");
+  //       }
+  //     }
 
-      System.out.println();
+  //     System.out.println();
      
-    }
+  //   }
 
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=5;j++){
-            if(j==5){
-                System.out.println("*");
-            }
-        }
-    }
-  }
+  //   for(int i=1;i<=5;i++){
+  //       for(int j=1;j<=5;j++){
+  //           if(j==5){
+  //               System.out.println("*");
+  //           }
+  //       }
+  //   }
+  // }
 
   public static void main(String[] args) {
     //    hollowRect(4,5 );
